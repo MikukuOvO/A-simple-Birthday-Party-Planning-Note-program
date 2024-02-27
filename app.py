3d7bb53753583775735435efc59f541f6406d950
MikukuOvO <mikukuovo@gmail.com>
2024-02-27 23:26:22 +0800

    test var pass

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def input_info():
     return render_template('basic_info.html')
 
+@app.route('/setplanday')
+def set_plan_day():
+    return render_template('set_plan_day.html')
+
 if __name__ == '__main__':
     app.run()
