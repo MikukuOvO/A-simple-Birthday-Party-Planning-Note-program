d330c0e73bfece707cf41140d46c700867f88456
MikukuOvO <mikukuovo@gmail.com>
2024-03-03 17:26:55 +0800

    Add database and information checkin

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+items = [
+    {"id": 1, "name": "小红", "relation": "朋友", "birthday": "1970-01-01"}, 
+    {"id": 2, "name": "小绿", "relation": "朋友", "birthday": "2050-01-01"}
+]
+
+@app.route('/api/items', methods=['GET'])
+def get_items():
+    return jsonify(items)
+
+@app.route('/api/items', methods=['POST'])
+def add_item():
+    item = request.json
+    item['id'] = len(items) + 1
+    items.append(item)
+    return jsonify(item), 201
+
+@app.route('/allinfo')
+def input_all_info():
+    return render_template('all_info.html')
 
 @app.route('/basicinfo')
 def input_info():
@@ -27,4 +49,6 @@ def closepage():
     return render_template('closepage.html')
 
 if __name__ == '__main__':
-    app.run()
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
